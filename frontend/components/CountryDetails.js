8e08f52b3365efac4c341edddbaa6848e0f01a74
Rohan Damani <rodamani10@gmail.com>
2024-10-02 16:29:27 -0500

    new country details page

--- frontend/components/CountryDetails.js
+++ frontend/components/CountryDetails.js
@@ -1,15 +1,39 @@
-export default function CountryDetails({ countryData }) {
+import styles from "../styles/CountryDetails.module.css";
+
+export default function CountryDetails({ countryData, countryDetails }) {
   if (countryData.length === 0) {
     return <p>Loading data...</p>;
   }
 
   return (
-    <ul>
-      {countryData.map((entry, index) => (
-        <li key={index}>
-          Year: {entry.year}, Total Refugees: {entry.total.toLocaleString()}
-        </li>
-      ))}
-    </ul>
+    <div className={styles.countryDetails}>
+      {countryDetails['flag'] && <img src={countryDetails['flag']} alt={`flag picture`} style={{ width: '100px' }} />}
+      <h3>Official Name: {countryDetails['officialName']}</h3>
+      <h3>Common Name: {countryDetails['commonName']}</h3>
+      <h5>Member of the UN: {countryDetails['unMember']}</h5>
+      <br></br>
+      
+      <p>Capital: {countryDetails['capital']}</p>
+      <p>Population: {countryDetails['population']}</p>
+      <p>Region: {countryDetails['region']}</p>
+      <p>Subregion: {countryDetails['subregion']}</p>
+
+      <div className={styles.yearlyStats}>
+        <ul>
+          {countryData.map((entry, index) => (
+            <li key={index}>
+              Year: {entry.year}, Total Refugees: {entry.total.toLocaleString()}
+            </li>
+          ))}
+        </ul>
+      </div>
+      
+      <div style={{ marginTop: '20px' }}>
+        <h4>Location:</h4>
+        <a href={countryDetails['maps']} target="_blank" rel="noopener noreferrer">
+          View on Google Maps
+        </a>
+      </div>
+    </div>
   );
 }
--- frontend/pages/country/[coa].js
+++ frontend/pages/country/[coa].js
@@ -3,11 +3,13 @@ import { useEffect, useState } from 'react';
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import CountryDetails from '../../components/CountryDetails';
+import axios from 'axios';
 
 export default function CountryPage() {
   const router = useRouter();
   const { coa } = router.query;
   const [countryData, setCountryData] = useState([]);
+  const [countryDetails, setCountryDetails] = useState({});
 
   useEffect(() => {
     if (!coa) return;
@@ -33,11 +35,36 @@ export default function CountryPage() {
       })
       .then(data => {
         setCountryData(data['items']);
+        fetchCountryDetails()
       })
       .catch(error => {
         console.error('Problem with fetch: ', error);
       });
   }, [coa]);
+
+  const fetchCountryDetails = async () => {
+    const details = {}
+
+    try {
+      const res = await axios.get(`https://restcountries.com/v3.1/alpha/${coa}`);
+      const countryData = res.data[0];
+
+      details['commonName'] = countryData.name.common;
+      details['officialName'] = countryData.name.official;
+      details['unMember'] = countryData.unMember ? "Yes" : "No";
+      details['flag'] = countryData.flags.png;
+      details['maps'] = countryData.maps.googleMaps;
+      details['capital'] = countryData.capital ? countryData.capital[0] : 'Unknown';
+      details['population'] = countryData.population.toLocaleString();
+      details['region'] = countryData.region;
+      details['subregion'] = countryData.subregion;
+
+    } catch (error) {
+      console.error(`Problem with country - ${coa}:`, error);
+    }
+
+    setCountryDetails(details);
+  };
 
   return (
     <div>
@@ -47,7 +74,10 @@ export default function CountryPage() {
       <main className={styles.mainContent}>
         <h1>Details for {coa}</h1>
 
-        <CountryDetails countryData={countryData} />
+        <CountryDetails
+          countryData={countryData}
+          countryDetails={countryDetails}
+        />
       </main>
     </div>
   );
